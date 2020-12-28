3ec8745ead830b9cf80dbf62f7376ea912933123
Dreamwalker <aristojeff@gmail.com>
2020-12-28 18:23:17 +0900

    Update monitoring_app.dart

--- lib/ep651_monitoring_app/monitoring_app.dart
+++ lib/ep651_monitoring_app/monitoring_app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
+import './page/monitoring_home_page.dart';
 
 class MonitoringApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+      home: MonitoringHomePage(),
 
     );
   }
