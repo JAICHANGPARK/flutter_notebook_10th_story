743e14b71e5f20173964ba670d9195cab3ef71ca
Dreamwalker <aristojeff@gmail.com>
2020-12-30 17:59:19 +0900

    Update monitoring_home_page.dart

--- lib/ep651_monitoring_app/page/monitoring_home_page.dart
+++ lib/ep651_monitoring_app/page/monitoring_home_page.dart
@@ -29,17 +29,18 @@ class _MonitoringHomePageState extends State<MonitoringHomePage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("To do today", style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 21,
-                    ),),
+                    Text(
+                      "To do today",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 21,
+                      ),
+                    ),
                     Padding(
                       padding: const EdgeInsets.symmetric(vertical: 8),
                       child: Container(
                         height: 100,
-                        color: Colors.grey
-                        ,
-
+                        color: Colors.grey,
                       ),
                     )
                   ],
