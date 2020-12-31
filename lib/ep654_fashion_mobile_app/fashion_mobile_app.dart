c810447c71d21fd9e6255e577a4dae77a949efe9
Dreamwalker <aristojeff@gmail.com>
2020-12-31 17:26:42 +0900

    Update fashion_mobile_app.dart

--- lib/ep654_fashion_mobile_app/fashion_mobile_app.dart
+++ lib/ep654_fashion_mobile_app/fashion_mobile_app.dart
@@ -1,1 +1,9 @@
-i
+import 'package:flutter/material.dart';
+
+
+class FashionMobileApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
