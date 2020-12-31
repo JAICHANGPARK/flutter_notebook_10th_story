7cf1e92f2b8c4bb560687ec775ba9e79dc2d4220
Dreamwalker <aristojeff@gmail.com>
2020-12-31 17:28:05 +0900

    Update fashion_mobile_app.dart

--- lib/ep654_fashion_mobile_app/fashion_mobile_app.dart
+++ lib/ep654_fashion_mobile_app/fashion_mobile_app.dart
@@ -4,6 +4,23 @@ import 'package:flutter/material.dart';
 class FashionMobileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        "/" : (context) => FashionHomePage(),
+      }
+    );
+  }
+}
+
+class FashionHomePage extends StatefulWidget {
+  @override
+  _FashionHomePageState createState() => _FashionHomePageState();
+}
+
+class _FashionHomePageState extends State<FashionHomePage> {
+  @override
+  Widget build(BuildContext context) {
     return Container();
   }
 }
+
