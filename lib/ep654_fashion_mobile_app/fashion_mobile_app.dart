b7a756ef32ae1cee7076bbf12f2508b0948d1004
Dreamwalker <aristojeff@gmail.com>
2020-12-31 17:38:11 +0900

    Update fashion_mobile_app.dart

--- lib/ep654_fashion_mobile_app/fashion_mobile_app.dart
+++ lib/ep654_fashion_mobile_app/fashion_mobile_app.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 class FashionMobileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        "/" : (context) => FashionHomePage(),
-      }
-    );
+    return MaterialApp(routes: {
+      "/": (context) => FashionHomePage(),
+    });
   }
 }
 
@@ -19,98 +16,68 @@ class FashionHomePage extends StatefulWidget {
 
 class _FashionHomePageState extends State<FashionHomePage> {
   int _homeIdx = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-       body: SafeArea(
-         child: Column(
-           children: [
-             Expanded(
-                 flex: 2,
-                 child: Container(
-                   padding: EdgeInsets.symmetric(horizontal: 16),
-                   child: TextField(
-          cursorColor: Colors.black,
-                     decoration: InputDecoration(
-                       icon: Icon(Icons.search),
-                       hintText: "Search Products",
-                       enabledBorder: OutlineInputBorder(
-                         borderSide: BorderSide(
-                           color: Colors.grey,
-                           width: 1.2
-                         )
-                       ),
-                       focusedBorder:  OutlineInputBorder(
-                           borderSide: BorderSide(
-                               color: Colors.grey,
-                               width: 1.2
-                           )
-                       ),
-                     ),
-                   ),
-                 )),
-             Expanded(
-                 flex: 3,
-                 child: Placeholder()),
-             Expanded(
-                 flex: 14,
-                 child: Placeholder()),
-           ],
-         ),
-
-       ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: "Search Products",
+                      enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 1.2)),
+                      focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey, width: 1.2)),
+                    ),
+                  ),
+                )),
+            Expanded(flex: 3, child: Placeholder()),
+            Expanded(flex: 14, child: Placeholder()),
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         showUnselectedLabels: false,
         showSelectedLabels: false,
         unselectedItemColor: Colors.grey,
         selectedItemColor: Colors.black,
         currentIndex: _homeIdx,
-
-        onTap: (homeIndex){
+        onTap: (homeIndex) {
           setState(() {
             _homeIdx = homeIndex;
           });
         },
         type: BottomNavigationBarType.fixed,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home_outlined,
-          ),
-          label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.apps_outlined,
-          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_outlined,
+              ),
               label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined,
-          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.apps_outlined,
+              ),
               label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.person_outline,
-          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.shopping_cart_outlined,
+              ),
               label: ""),
-
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_outline,
+              ),
+              label: ""),
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
