15ad2377d81c2dd2051a6ee553bed8027896719f
Dreamwalker <aristojeff@gmail.com>
2021-01-02 17:28:10 +0900

    Create kids_collections_page.dart

---
+++ lib/ep654_fashion_mobile_app/kids_collections_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class KidsCollectionsPage extends StatefulWidget {
+  @override
+  _KidsCollectionsPageState createState() => _KidsCollectionsPageState();
+}
+
+class _KidsCollectionsPageState extends State<KidsCollectionsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
