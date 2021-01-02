b78d70412d766acebf4c4470c3e0d42a90246342
Dreamwalker <aristojeff@gmail.com>
2021-01-02 17:30:37 +0900

    Update kids_collections_page.dart

--- lib/ep654_fashion_mobile_app/kids_collections_page.dart
+++ lib/ep654_fashion_mobile_app/kids_collections_page.dart
@@ -8,6 +8,43 @@ class KidsCollectionsPage extends StatefulWidget {
 class _KidsCollectionsPageState extends State<KidsCollectionsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ,
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.black,
+        currentIndex: _homeIdx,
+        onTap: (homeIndex) {
+          setState(() {
+            _homeIdx = homeIndex;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_outlined,
+              ),
+              label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.apps_outlined,
+              ),
+              label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.shopping_cart_outlined,
+              ),
+              label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_outline,
+              ),
+              label: ""),
+        ],
+      ),
+    );
   }
 }
