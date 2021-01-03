009281b57bea3155cf94ceb09bce3a594c15bdcc
Dreamwalker <aristojeff@gmail.com>
2021-01-03 17:25:43 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'ep654_fashion_mobile_app/fashion_mobile_app.dart';
+import 'ep657_yoga_app/yoga_contents_app.dart';
 
 void main() {
-  runApp(FashionMobileApp());
+  runApp(YogaContentsApp());
 }
