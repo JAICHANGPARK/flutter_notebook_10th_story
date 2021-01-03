ba22d88a0d7a0700e540c736c6e9ff3512e5c2da
Dreamwalker <aristojeff@gmail.com>
2021-01-03 17:25:29 +0900

    Update yoga_home_page.dart

---
+++ lib/ep657_yoga_app/ui/yoga_home_page.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/material.dart';
+
+
+class YogaHomePage extends StatefulWidget {
+  @override
+  _YogaHomePageState createState() => _YogaHomePageState();
+}
+
+class _YogaHomePageState extends State<YogaHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
