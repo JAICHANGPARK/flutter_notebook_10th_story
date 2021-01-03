6421944bbf16457647a63ec8342d213944744604
Dreamwalker <aristojeff@gmail.com>
2021-01-03 17:27:43 +0900

    Update yoga_home_page.dart

--- lib/ep657_yoga_app/ui/yoga_home_page.dart
+++ lib/ep657_yoga_app/ui/yoga_home_page.dart
@@ -11,6 +11,24 @@ class YogaHomePage extends StatefulWidget {
 class _YogaHomePageState extends State<YogaHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
