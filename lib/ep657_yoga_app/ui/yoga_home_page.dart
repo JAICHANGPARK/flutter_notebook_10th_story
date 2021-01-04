37d631f26f841dc66e27feaeb7a1052a3df01e0d
Dreamwalker <aristojeff@gmail.com>
2021-01-04 19:54:59 +0900

    Update yoga_home_page.dart

--- lib/ep657_yoga_app/ui/yoga_home_page.dart
+++ lib/ep657_yoga_app/ui/yoga_home_page.dart
@@ -18,169 +18,183 @@ class _YogaHomePageState extends State<YogaHomePage> with SingleTickerProviderSt
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            child: NestedScrollView(
-              headerSliverBuilder: (context, innerBoxIsScrolled) {
-                return [
-                  SliverAppBar(
-                    floating: true,
-                    pinned: true,
-                    snap: true,
-                    expandedHeight: 200,
-                    forceElevated: innerBoxIsScrolled,
-                    backgroundColor: Colors.white,
-                    // title: Text(
-                    //   "Welcome Dream Walker",
-                    //   style: TextStyle(
-                    //     color: Colors.black,
-                    //   ),
-                    // ),
-                    // actions: [
-                    //   Padding(
-                    //     padding: const EdgeInsets.all(8.0),
-                    //     child: CircleAvatar(
-                    //       radius: 24,
-                    //     ),
-                    //   ),
-                    // ],
-                    flexibleSpace: FlexibleSpaceBar(
-                      collapseMode: CollapseMode.pin,
-                      background: Column(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: NestedScrollView(
+                headerSliverBuilder: (context, innerBoxIsScrolled) {
+                  return [
+                    SliverAppBar(
+                      floating: true,
+                      pinned: true,
+                      snap: true,
+                      expandedHeight: 200,
+                      forceElevated: innerBoxIsScrolled,
+                      backgroundColor: Colors.white,
+
+                      actions: [
+
+                      ],
+                      flexibleSpace: FlexibleSpaceBar(
+                        collapseMode: CollapseMode.parallax,
+
+                        background: Column(
+                          children: [
+                            Container(
+                              height: 64,
+                              decoration: BoxDecoration(color: Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: Row(
+                                children: [
+                                Text(
+                                "Welcome Dream Walker",
+                                style: TextStyle(
+                  color: Colors.black,
+                  ),),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: CircleAvatar(
+                                      radius: 24,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              height: 64,
+                              decoration: BoxDecoration(color: Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(16)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      bottom: TabBar(
+                          labelColor: Colors.orange,
+                          unselectedLabelColor: Colors.grey,
+                          indicatorColor: Colors.orange,
+                          isScrollable: false,
+                          indicatorSize: TabBarIndicatorSize.label,
+                          labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                          controller: _tabController,
+                          tabs: [
+                            Tab(
+                              text: "Subscribed",
+                            ),
+                            Tab(
+                              text: "Trending",
+                            )
+                          ]),
+                      // bottom: PreferredSize(
+                      //
+                      //   preferredSize: Size.fromHeight(140),
+                      //   child: Column(
+                      //     children: [
+                      //       Padding(
+                      //         padding: const EdgeInsets.symmetric(horizontal: 16),
+                      //         child: Container(
+                      //           height: 64,
+                      //           decoration: BoxDecoration(color: Colors.grey[200], borderRadius: BorderRadius.circular(16)),
+                      //         ),
+                      //       ),
+                      //       Padding(
+                      //         padding: const EdgeInsets.only(top: 16),
+                      //         child: TabBar(
+                      //             labelColor: Colors.orange,
+                      //             unselectedLabelColor: Colors.grey,
+                      //             indicatorColor: Colors.orange,
+                      //             isScrollable: false,
+                      //             indicatorSize: TabBarIndicatorSize.label,
+                      //             labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                      //             controller: _tabController,
+                      //             tabs: [
+                      //               Tab(
+                      //                 text: "Subscribed",
+                      //               ),
+                      //               Tab(
+                      //                 text: "Trending",
+                      //               )
+                      //             ]),
+                      //       )
+                      //     ],
+                      //   ),
+                      // ),
+                    )
+                  ];
+                },
+                body: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    Container(
+                      color: Colors.redAccent,
+                    ),
+                    Container(
+                      color: Colors.blue,
+                    ),
+                    // Container(
+                    //   color: Colors.green,
+                    // )
+                  ],
+                ),
+                // slivers: [
+                //
+                //   SliverList(delegate:
+                //   SliverChildBuilderDelegate(
+                //       (context, index) => ListTile(title: Text("Sample"),)
+                //   ))
+                // ],
+              ),
+            ),
+            Positioned(
+              left: 24,
+              right: 24,
+              bottom: 24,
+              child: Card(
+                elevation: 8,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
                         children: [
-                          Container(
-                            height: 64,
-                            decoration: BoxDecoration(color: Colors.grey[300],
-                                borderRadius: BorderRadius.circular(16)),
+                          IconButton(
+                            icon: Icon(Icons.home_outlined),
+                            color: Colors.orange,
+                            iconSize: 32,
+                            onPressed: () {},
+                          ),
+                          Text(
+                            "Home",
+                            style: TextStyle(color: Colors.orange),
                           ),
                         ],
                       ),
-                    ),
-                    bottom: TabBar(
-                        labelColor: Colors.orange,
-                        unselectedLabelColor: Colors.grey,
-                        indicatorColor: Colors.orange,
-                        isScrollable: false,
-                        indicatorSize: TabBarIndicatorSize.label,
-                        labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                        controller: _tabController,
-                        tabs: [
-                          Tab(
-                            text: "Subscribed",
-                          ),
-                          Tab(
-                            text: "Trending",
-                          )
-                        ]),
-                    // bottom: PreferredSize(
-                    //
-                    //   preferredSize: Size.fromHeight(140),
-                    //   child: Column(
-                    //     children: [
-                    //       Padding(
-                    //         padding: const EdgeInsets.symmetric(horizontal: 16),
-                    //         child: Container(
-                    //           height: 64,
-                    //           decoration: BoxDecoration(color: Colors.grey[200], borderRadius: BorderRadius.circular(16)),
-                    //         ),
-                    //       ),
-                    //       Padding(
-                    //         padding: const EdgeInsets.only(top: 16),
-                    //         child: TabBar(
-                    //             labelColor: Colors.orange,
-                    //             unselectedLabelColor: Colors.grey,
-                    //             indicatorColor: Colors.orange,
-                    //             isScrollable: false,
-                    //             indicatorSize: TabBarIndicatorSize.label,
-                    //             labelStyle: TextStyle(fontWeight: FontWeight.bold),
-                    //             controller: _tabController,
-                    //             tabs: [
-                    //               Tab(
-                    //                 text: "Subscribed",
-                    //               ),
-                    //               Tab(
-                    //                 text: "Trending",
-                    //               )
-                    //             ]),
-                    //       )
-                    //     ],
-                    //   ),
-                    // ),
-                  )
-                ];
-              },
-              body: TabBarView(
-                controller: _tabController,
-                children: [
-                  Container(
-                    color: Colors.redAccent,
+                      IconButton(
+                        icon: Icon(Icons.add_chart),
+                        color: Colors.black,
+                        iconSize: 32,
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.person_outline),
+                        color: Colors.black,
+                        iconSize: 32,
+                        onPressed: () {},
+                      ),
+                    ],
                   ),
-                  Container(
-                    color: Colors.blue,
-                  ),
-                  // Container(
-                  //   color: Colors.green,
-                  // )
-                ],
-              ),
-              // slivers: [
-              //
-              //   SliverList(delegate:
-              //   SliverChildBuilderDelegate(
-              //       (context, index) => ListTile(title: Text("Sample"),)
-              //   ))
-              // ],
-            ),
-          ),
-          Positioned(
-            left: 24,
-            right: 24,
-            bottom: 24,
-            child: Card(
-              elevation: 8,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Row(
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.home_outlined),
-                          color: Colors.orange,
-                          iconSize: 32,
-                          onPressed: () {},
-                        ),
-                        Text(
-                          "Home",
-                          style: TextStyle(color: Colors.orange),
-                        ),
-                      ],
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.add_chart),
-                      color: Colors.black,
-                      iconSize: 32,
-                      onPressed: () {},
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.person_outline),
-                      color: Colors.black,
-                      iconSize: 32,
-                      onPressed: () {},
-                    ),
-                  ],
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
