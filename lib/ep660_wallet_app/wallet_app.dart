2db3ff4d0b0a7e234b8178663b40a5063f0744c7
Dreamwalker <aristojeff@gmail.com>
2021-01-06 20:45:29 +0900

    Update wallet_app.dart

--- lib/ep660_wallet_app/wallet_app.dart
+++ lib/ep660_wallet_app/wallet_app.dart
@@ -1,2 +1,23 @@
 import 'package:flutter/material.dart';
 
+class WalletApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WalletAppHomePage(),
+    );
+  }
+}
+
+class WalletAppHomePage extends StatefulWidget {
+  @override
+  _WalletAppHomePageState createState() => _WalletAppHomePageState();
+}
+
+class _WalletAppHomePageState extends State<WalletAppHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
