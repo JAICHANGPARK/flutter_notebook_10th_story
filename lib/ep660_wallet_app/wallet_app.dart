4e850f56a8de7c8049d743704d9022f10a0e35e2
Dreamwalker <aristojeff@gmail.com>
2021-01-08 20:00:33 +0900

    Update wallet_app.dart

--- lib/ep660_wallet_app/wallet_app.dart
+++ lib/ep660_wallet_app/wallet_app.dart
@@ -263,7 +263,8 @@ class _WalletAppHomePageState extends State<WalletAppHomePage> {
                       height: 8,
                     ),
                     Card(
-                      child: Column(
+                      child: ListView(
+                        shrinkWrap: true,
                         children: [
                           Padding(
                             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
@@ -298,6 +299,92 @@ class _WalletAppHomePageState extends State<WalletAppHomePage> {
                                 Spacer(),
                                 Text(
                                   "- 2,560.00 EUR",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Divider(
+                            color: Colors.grey,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: 42,
+                                  width: 42,
+                                  decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Enato Market",
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      Text(
+                                        "Card - Mike transfer",
+                                        style: TextStyle(fontSize: 11, color: Colors.grey),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Spacer(),
+                                Text(
+                                  "- 560.00 EUR",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Divider(
+                            color: Colors.grey,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: 42,
+                                  width: 42,
+                                  decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Enato Market",
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                      Text(
+                                        "Card - Mike transfer",
+                                        style: TextStyle(fontSize: 11, color: Colors.grey),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Spacer(),
+                                Text(
+                                  "- 560.00 EUR",
                                   style: TextStyle(
                                     fontSize: 20,
                                   ),
