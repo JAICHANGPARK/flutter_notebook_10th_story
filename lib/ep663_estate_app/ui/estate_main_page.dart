a7dfb48bc06a4b14a9cbdf59e6df9c8263320805
Dreamwalker <aristojeff@gmail.com>
2021-01-09 17:26:19 +0900

    Update estate_main_page.dart

---
+++ lib/ep663_estate_app/ui/estate_main_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EstateMainPage extends StatefulWidget {
+  @override
+  _EstateMainPageState createState() => _EstateMainPageState();
+}
+
+class _EstateMainPageState extends State<EstateMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
