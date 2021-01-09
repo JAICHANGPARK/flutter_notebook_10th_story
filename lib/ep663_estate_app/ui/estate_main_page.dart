714f36fc37bc6a1b6c301b400fa580c68b6b9b70
Dreamwalker <aristojeff@gmail.com>
2021-01-09 17:31:41 +0900

    Update estate_main_page.dart

--- lib/ep663_estate_app/ui/estate_main_page.dart
+++ lib/ep663_estate_app/ui/estate_main_page.dart
@@ -9,29 +9,36 @@ class _EstateMainPageState extends State<EstateMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Stack(
+      body: SafeArea(
+          child: Stack(
         children: [
-          Positioned(child: Placeholder(),
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: MediaQuery.of(context).size.height / 2.3,),
-          Positioned(child: Placeholder(),
+          Positioned(
+            child: Placeholder(),
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: MediaQuery.of(context).size.height / 2.3,
+          ),
+          Positioned(
+            child: Placeholder(),
             top: MediaQuery.of(context).size.height / 2.8,
             left: 0,
             right: 0,
-            bottom: 0,),
-
+            bottom: 0,
+          ),
         ],
-
       )),
       bottomNavigationBar: BottomAppBar(
-
         child: Container(
           height: 64,
           child: Row(
             children: [
-
+              Column(
+                children: [IconButton(icon: Icon(Icons.home_outlined),
+                    onPressed: () {
+                  
+                    })],
+              )
             ],
           ),
         ),
@@ -39,3 +46,18 @@ class _EstateMainPageState extends State<EstateMainPage> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
