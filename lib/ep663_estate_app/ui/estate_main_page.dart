b06d029f259816b033bd2cb121a112d6502e811c
Dreamwalker <aristojeff@gmail.com>
2021-01-11 19:52:09 +0900

    Update estate_main_page.dart

--- lib/ep663_estate_app/ui/estate_main_page.dart
+++ lib/ep663_estate_app/ui/estate_main_page.dart
@@ -132,10 +132,8 @@ class _EstateMainPageState extends State<EstateMainPage> {
                             decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(8)),
                             padding: EdgeInsets.only(left: 16),
                             child: TextField(
-                              decoration: InputDecoration(icon: Icon(Icons.search),
-
-                                  hintText: "Search Properties...",
-                              border: InputBorder.none),
+                              decoration: InputDecoration(
+                                  icon: Icon(Icons.search), hintText: "Search Properties...", border: InputBorder.none),
                             ),
                           ),
                         ),
@@ -146,8 +144,10 @@ class _EstateMainPageState extends State<EstateMainPage> {
                             flex: 2,
                             child: Container(
                               height: 52,
-                              decoration:
-                                  BoxDecoration(color: Colors.deepOrange, borderRadius: BorderRadius.circular(8)),
+                              decoration: BoxDecoration(
+                                color: Colors.deepOrange,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
                             )),
                       ],
                     ),
