b8777861235ce26048f0e845a5aa60137044d40a
Dreamwalker <aristojeff@gmail.com>
2021-01-13 19:57:17 +0900

    Update layout_0_page.dart

--- lib/ep667_mobile_layout/layout_0_page.dart
+++ lib/ep667_mobile_layout/layout_0_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'mobile_layout_app.dart';
 
 class Layout0Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: mobileLayoutAppColor,
       body: Column(
         children: [
 
