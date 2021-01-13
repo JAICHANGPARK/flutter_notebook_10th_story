73e17e192e3a008a530afd185be4e60640d05cca
Dreamwalker <aristojeff@gmail.com>
2021-01-13 19:55:22 +0900

    Update mobile_layout_app.dart

--- lib/ep667_mobile_layout/mobile_layout_app.dart
+++ lib/ep667_mobile_layout/mobile_layout_app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
+Color mobileLayoutAppColor = Color(0xff040dfd);
 class MobileLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+
     );
   }
 }
