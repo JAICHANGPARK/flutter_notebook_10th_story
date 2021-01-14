5f4c71af523e4e3be6ae1ac0edbc34cac37ed801
Dreamwalker <aristojeff@gmail.com>
2021-01-14 20:15:01 +0900

    Update layout_1_page.dart

--- lib/ep667_mobile_layout/layout_1_page.dart
+++ lib/ep667_mobile_layout/layout_1_page.dart
@@ -4,6 +4,14 @@ import 'package:flutter/material.dart';
 class Layout1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("6" ,style: TextStyle(
+            fontSize: 120
+          ),)
+        ],
+      ),
+    );
   }
 }
