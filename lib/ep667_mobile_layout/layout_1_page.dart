f66e0ebda96501ee142c6cb13193d0bb49586969
Dreamwalker <aristojeff@gmail.com>
2021-01-14 20:16:41 +0900

    Update layout_1_page.dart

--- lib/ep667_mobile_layout/layout_1_page.dart
+++ lib/ep667_mobile_layout/layout_1_page.dart
@@ -8,13 +8,17 @@ class Layout1Page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(
-            height: 48,
+            height: 280,
           ),
           Text("6" ,style: TextStyle(
             fontSize: 280,
-            color: mobileLayoutAppColor
+            color: mobileLayoutAppColor,
+            letterSpacing: 0,
+            wordSpacing: 0,
+            height: 0
           ),),
           Divider(
             color: mobileLayoutAppColor,
