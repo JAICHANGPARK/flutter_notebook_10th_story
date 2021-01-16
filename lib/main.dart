743cb10cecbf5bdccec132c786c5a5d0473a73a3
Dreamwalker <aristojeff@gmail.com>
2021-01-16 17:59:30 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_10th_story/ep667_mobile_layout/mobile_layout_app.dart';
+import 'ep670_online_doctor_app/online_doctor_app.dart';
 
 void main() {
-  runApp(MobileLayout());
+  runApp(OnlineDoctorApp());
 }
