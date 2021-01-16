44bfa6cf9bd3e872c48f32e5f1668c757d4da2e2
Dreamwalker <aristojeff@gmail.com>
2021-01-16 18:05:33 +0900

    Update onlin_doctor_home_page.dart

--- lib/ep670_online_doctor_app/ui/onlin_doctor_home_page.dart
+++ lib/ep670_online_doctor_app/ui/onlin_doctor_home_page.dart
@@ -6,23 +6,35 @@ class OnlineDoctorHomePage extends StatefulWidget {
 }
 
 class _OnlineDoctorHomePageState extends State<OnlineDoctorHomePage> {
+
+  Widget _buildBackgroundWidget(){
+    return Positioned(
+      left: 0,
+      top: 0,
+      bottom: 0,
+      right: 0,
+      child: Row(
+        children: [
+          Expanded(
+              flex: 8,
+              child: Container(
+                color: Colors.white,
+              )),
+          Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.grey[200],
+              )),
+        ],
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            child: Row(
-              children: [
-                Expanded(flex: 8, child: Container(
-                  color: Colors.white,
-                )),
-                Expanded(flex: 3, child: Container(
-                  color: Colors.grey[200],
-                )),
-              ],
-            ),
-          ),
+          _buildBackgroundWidget(),
         ],
       ),
     );
