9b17cb7a6a4987fa9f614987e65184b1f1fd6fc6
Dreamwalker <aristojeff@gmail.com>
2021-01-20 19:55:50 +0900

    Update doctor_detail_page.dart

---
+++ lib/ep670_online_doctor_app/ui/doctor_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class DoctorDetailPage extends StatefulWidget {
+  S
+  @override
+  _DoctorDetailPageState createState() => _DoctorDetailPageState();
+}
+
+class _DoctorDetailPageState extends State<DoctorDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
