d53e7fba1e39e20f0f72e894d250854377b3895f
Dreamwalker <aristojeff@gmail.com>
2021-01-23 18:33:03 +0900

    Create art_main_page.dart

---
+++ lib/ep678_art_preview_app/ui/art_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ArtMainPage extends StatefulWidget {
+  ArtMainPage({Key key}) : super(key: key);
+
+  @override
+  _ArtMainPageState createState() => _ArtMainPageState();
+}
+
+class _ArtMainPageState extends State<ArtMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
