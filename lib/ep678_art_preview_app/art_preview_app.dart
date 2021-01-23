1ba80e3f7b7c744579bac28ca9213b4befea2129
Dreamwalker <aristojeff@gmail.com>
2021-01-23 18:33:35 +0900

    Update art_preview_app.dart

--- lib/ep678_art_preview_app/art_preview_app.dart
+++ lib/ep678_art_preview_app/art_preview_app.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_10th_story/ep678_art_preview_app/ui/art_main_page.dart';
 
 class ArtPreviewApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      routes: {
+        "/" : (context) => ArtMainPage()
+      },
+    );
   }
 }
 
