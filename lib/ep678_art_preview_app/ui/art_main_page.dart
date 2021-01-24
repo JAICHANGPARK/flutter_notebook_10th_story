f124ec847d028c56637919817125d4d93aaf87ec
Dreamwalker <aristojeff@gmail.com>
2021-01-24 18:25:46 +0900

    Update art_main_page.dart

--- lib/ep678_art_preview_app/ui/art_main_page.dart
+++ lib/ep678_art_preview_app/ui/art_main_page.dart
@@ -162,29 +162,38 @@ class _ArtMainPageState extends State<ArtMainPage> {
               )),
               Expanded(
                   child: Padding(
-                padding: const EdgeInsets.only(left: 12, right: 12, bottom: 12),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 6),
-                      child: Container(
-                        height: 2,
-                        decoration: BoxDecoration(color: Colors.blue),
+                    padding: const EdgeInsets.only(left: 12, right: 12, bottom: 12),
+                    child: GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          _pageIndex = 0;
+                        });
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 6),
+                            child: Container(
+                              height: 2,
+                              decoration: BoxDecoration(color:
+                              _pageIndex == 0 ?
+                              Colors.blue : Colors.transparent),
+                            ),
+                          ),
+                          Icon(
+                            Icons.home_outlined,
+                            size: 38,
+                            color: _pageIndex == 0 ? Colors.blue : Colors.grey,
+                          ),
+                          Text(
+                            "Library",
+                            style: TextStyle(color: _pageIndex == 0 ? Colors.blue: Colors.grey),
+                          )
+                        ],
                       ),
                     ),
-                    Icon(
-                      Icons.home_outlined,
-                      size: 38,
-                      color: Colors.blue,
-                    ),
-                    Text(
-                      "Library",
-                      style: TextStyle(color: Colors.blue),
-                    )
-                  ],
-                ),
-              )),
+                  )),
               Expanded(child: Container()),
               Expanded(
                   child: Padding(
