2c5c778bb3707272cc5c53411add7a504d4f9ef7
Dreamwalker <aristojeff@gmail.com>
2021-01-25 20:07:25 +0900

    Update art.dart

---
+++ lib/ep678_art_preview_app/model/art.dart
@@ -0,0 +1,13 @@
+
+
+class Art{
+  final String img;
+  final String name;
+  final String detail;
+  final String width;
+  final String height;
+  final bool isFeature;
+
+  Art({this.img, this.name, this.detail, this.width, this.height, this.isFeature});
+
+}
