6f25449e2d81b7a963deb28b9b3b80ec4d42e0b5
Dreamwalker <aristojeff@gmail.com>
2021-01-27 20:20:35 +0900

    Update art_detail_page.dart

---
+++ lib/ep678_art_preview_app/ui/art_detail_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_10th_story/ep678_art_preview_app/model/art.dart';
+
+class ArtDetailPage extends StatefulWidget {
+  final Art art;
+
+  ArtDetailPage({this.art});
+
+  @override
+  _ArtDetailPageState createState() => _ArtDetailPageState();
+}
+
+class _ArtDetailPageState extends State<ArtDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
