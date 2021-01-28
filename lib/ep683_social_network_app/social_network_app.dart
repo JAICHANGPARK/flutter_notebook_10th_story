beef27ac5774b699895513b4b3693e7689f23dde
Dreamwalker <aristojeff@gmail.com>
2021-01-28 19:44:17 +0900

    Update social_network_app.dart

--- lib/ep683_social_network_app/social_network_app.dart
+++ lib/ep683_social_network_app/social_network_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'ui/social_network_feed_page.dart';
+
 class SocialNetworkApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
