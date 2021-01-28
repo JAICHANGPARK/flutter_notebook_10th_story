c729fe04cb80089142dd614440146117a3b8f7d0
Dreamwalker <aristojeff@gmail.com>
2021-01-28 19:44:43 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'ep678_art_preview_app/art_preview_app.dart';
+
+import 'ep683_social_network_app/social_network_app.dart';
 
 void main() {
-  runApp(ArtPreviewApp());
+  runApp(SocialNetworkApp());
 }
