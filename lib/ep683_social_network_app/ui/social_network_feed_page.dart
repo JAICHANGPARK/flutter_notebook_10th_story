f68b534696f0d0b0c9ccb6c7d03dd04f5ce4a457
Dreamwalker <aristojeff@gmail.com>
2021-01-28 19:48:33 +0900

    Update social_network_feed_page.dart

--- lib/ep683_social_network_app/ui/social_network_feed_page.dart
+++ lib/ep683_social_network_app/ui/social_network_feed_page.dart
@@ -9,10 +9,26 @@ class _SocialNetworkFeedPageState extends State<SocialNetworkFeedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Column(
         children: [
-
+          //app bar
+          Expanded(
+              flex: 1,
+              child: Placeholder()),
+          //Live
+          Expanded(
+              flex: 3,
+              child: Placeholder()),
+          //Tab
+          Expanded(
+              flex: 3,child: Placeholder()),
+          //Feed
+          Expanded(
+              flex: 8,
+              child: Placeholder()),
+          //Botttom
+          Expanded(
+              flex: 1,child: Placeholder()),
         ],
       ),
     );
