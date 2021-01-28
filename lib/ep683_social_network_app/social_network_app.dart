68f1e1f5663f786c4c8101d107f5e7aada45e7b8
Dreamwalker <aristojeff@gmail.com>
2021-01-28 19:45:17 +0900

    Update social_network_app.dart

--- lib/ep683_social_network_app/social_network_app.dart
+++ lib/ep683_social_network_app/social_network_app.dart
@@ -6,6 +6,9 @@ class SocialNetworkApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        accentColor: Colors.yellow
+      ),
       home: SocialNetworkFeedPage(),
     );
   }
