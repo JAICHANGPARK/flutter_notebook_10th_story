98aa32a3cac508ecc9bc1dec8a9e68418ef33c9e
Dreamwalker <aristojeff@gmail.com>
2021-01-28 19:44:13 +0900

    Update social_network_feed_page.dart

--- lib/ep683_social_network_app/ui/social_network_feed_page.dart
+++ lib/ep683_social_network_app/ui/social_network_feed_page.dart
@@ -1,2 +1,13 @@
 import 'package:flutter/material.dart';
 
+class SocialNetworkFeedPage extends StatefulWidget {
+  @override
+  _SocialNetworkFeedPageState createState() => _SocialNetworkFeedPageState();
+}
+
+class _SocialNetworkFeedPageState extends State<SocialNetworkFeedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
