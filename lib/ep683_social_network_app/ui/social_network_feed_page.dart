622f5b7ee7c4044458cb1060dc48e7e5e531839a
Dreamwalker <aristojeff@gmail.com>
2021-01-29 20:11:51 +0900

    Update social_network_feed_page.dart

--- lib/ep683_social_network_app/ui/social_network_feed_page.dart
+++ lib/ep683_social_network_app/ui/social_network_feed_page.dart
@@ -34,83 +34,95 @@ class _SocialNetworkFeedPageState extends State<SocialNetworkFeedPage> {
                   ],
                 )),
             //Live
-            Expanded(flex: 3, child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                Container(
-                  width: 100,
-                  margin: EdgeInsets.all(7),
-                  decoration: BoxDecoration(
-                    color: Colors.pink,
-                    borderRadius: BorderRadius.circular(8),
-                    image: DecorationImage(
-                      image: NetworkImage("https://cdn.pixabay.com/photo/2018/03/12/20/57/portrait-3220832__340.jpg"),
-                      fit: BoxFit.cover,
-                      colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2 ), BlendMode.darken)
-                    )
-                  ),
-                  child: Center(
-                    child: CircleAvatar(
-                      backgroundColor: Colors.white.withOpacity(0.32),
-                      child: Icon(Icons.camera_alt, color: Colors.white,),
+            Expanded(
+                flex: 3,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Container(
+                      width: 100,
+                      margin: EdgeInsets.all(7),
+                      decoration: BoxDecoration(
+                          color: Colors.pink,
+                          borderRadius: BorderRadius.circular(8),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://cdn.pixabay.com/photo/2018/03/12/20/57/portrait-3220832__340.jpg"),
+                              fit: BoxFit.cover,
+                              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.darken))),
+                      child: Center(
+                        child: CircleAvatar(
+                          backgroundColor: Colors.white.withOpacity(0.32),
+                          child: Icon(
+                            Icons.camera_alt,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                )
-                ,
-                Container(
-                  width: 100,
-                  margin: EdgeInsets.all(7),
-                  decoration: BoxDecoration(
-                      color: Colors.pink,
-                      borderRadius: BorderRadius.circular(8),
-                      image: DecorationImage(
-                          image: NetworkImage("https://cdn.pixabay.com/photo/2017/04/21/21/45/dj-2250056__340.jpg"),
-                          fit: BoxFit.cover,
-                          colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2 ), BlendMode.darken)
-                      )
-                  ),
-                ),
-                Container(
-                  width: 100,
-                  margin: EdgeInsets.all(7),
-                  decoration: BoxDecoration(
-                      color: Colors.pink,
-                      borderRadius: BorderRadius.circular(8),
-                      image: DecorationImage(
-                          image: NetworkImage("https://cdn.pixabay.com/photo/2019/12/17/04/53/neon-4700730__340.jpg"),
-                          fit: BoxFit.cover,
-                          colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2 ), BlendMode.darken)
-                      )
-                  ),
-                ),
-                Container(
-                  width: 100,
-                  margin: EdgeInsets.all(7),
-                  decoration: BoxDecoration(
-                      color: Colors.pink,
-                      borderRadius: BorderRadius.circular(8),
-                      image: DecorationImage(
-                          image: NetworkImage("https://cdn.pixabay.com/photo/2017/03/16/00/10/dj-2147859__340.jpg"),
-                          fit: BoxFit.cover,
-                          colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2 ), BlendMode.darken)
-                      )
-                  ),
-                ),
-                Container(
-                  width: 100,
-                  margin: EdgeInsets.all(7),
-                  decoration: BoxDecoration(
-                      color: Colors.pink,
-                      borderRadius: BorderRadius.circular(8),
-                      image: DecorationImage(
-                          image: NetworkImage("https://cdn.pixabay.com/photo/2019/12/17/04/52/neon-4700726__340.jpg"),
-                          fit: BoxFit.cover,
-                          colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2 ), BlendMode.darken)
-                      )
-                  ),
-                ),
-              ],
-            )),
+                    Container(
+                      width: 100,
+                      margin: EdgeInsets.all(7),
+                      decoration: BoxDecoration(
+                          color: Colors.pink,
+                          borderRadius: BorderRadius.circular(8),
+                          image: DecorationImage(
+                              image: NetworkImage("https://cdn.pixabay.com/photo/2017/04/21/21/45/dj-2250056__340.jpg"),
+                              fit: BoxFit.cover,
+                              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.darken))),
+                    ),
+                    Container(
+                      width: 100,
+                      margin: EdgeInsets.all(7),
+                      decoration: BoxDecoration(
+                          color: Colors.pink,
+                          borderRadius: BorderRadius.circular(8),
+                          image: DecorationImage(
+                              image:
+                                  NetworkImage("https://cdn.pixabay.com/photo/2019/12/17/04/53/neon-4700730__340.jpg"),
+                              fit: BoxFit.cover,
+                              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.darken)),
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                              right: 8,
+                              top: 8,
+                              child: Container(
+                            child: Center(
+                              child: Text("LIVE",style: TextStyle(
+                                fontSize: 12
+                              ),),
+                            ),
+                          ))
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      margin: EdgeInsets.all(7),
+                      decoration: BoxDecoration(
+                          color: Colors.pink,
+                          borderRadius: BorderRadius.circular(8),
+                          image: DecorationImage(
+                              image: NetworkImage("https://cdn.pixabay.com/photo/2017/03/16/00/10/dj-2147859__340.jpg"),
+                              fit: BoxFit.cover,
+                              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.darken))),
+                    ),
+                    Container(
+                      width: 100,
+                      margin: EdgeInsets.all(7),
+                      decoration: BoxDecoration(
+                          color: Colors.pink,
+                          borderRadius: BorderRadius.circular(8),
+                          image: DecorationImage(
+                              image:
+                                  NetworkImage("https://cdn.pixabay.com/photo/2019/12/17/04/52/neon-4700726__340.jpg"),
+                              fit: BoxFit.cover,
+                              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.darken))),
+                    ),
+                  ],
+                )),
             //Tab
             Expanded(flex: 3, child: Placeholder()),
             //Feed
@@ -120,8 +132,9 @@ class _SocialNetworkFeedPageState extends State<SocialNetworkFeedPage> {
                 flex: 1,
                 child: Row(
                   children: [
-                    Expanded(child: GestureDetector(
-                      onTap: (){
+                    Expanded(
+                        child: GestureDetector(
+                      onTap: () {
                         setState(() {
                           _tabIndex = 0;
                         });
@@ -130,34 +143,52 @@ class _SocialNetworkFeedPageState extends State<SocialNetworkFeedPage> {
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
                           Text("Notification"),
-                          _tabIndex == 0 ?  CircleAvatar(radius: 4,
-                            backgroundColor: Theme.of(context).accentColor,) : Container()
+                          _tabIndex == 0
+                              ? CircleAvatar(
+                                  radius: 4,
+                                  backgroundColor: Theme.of(context).accentColor,
+                                )
+                              : Container()
                         ],
                       ),
                     )),
-                    Expanded(child: GestureDetector( onTap: (){
-                      setState(() {
-                        _tabIndex = 1;
-                      });
-                    },
-                      child: Column(  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    Expanded(
+                        child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _tabIndex = 1;
+                        });
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
                           Text("Home"),
-                          _tabIndex == 1 ?  CircleAvatar(radius: 4,
-                            backgroundColor: Theme.of(context).accentColor,) : Container()
+                          _tabIndex == 1
+                              ? CircleAvatar(
+                                  radius: 4,
+                                  backgroundColor: Theme.of(context).accentColor,
+                                )
+                              : Container()
                         ],
                       ),
                     )),
-                    Expanded(child: GestureDetector( onTap: (){
-                      setState(() {
-                        _tabIndex = 2;
-                      });
-                    },
-                      child: Column(  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    Expanded(
+                        child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _tabIndex = 2;
+                        });
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
                           Text("Messages"),
-                          _tabIndex == 2 ?  CircleAvatar(radius: 4,
-                            backgroundColor: Theme.of(context).accentColor,) : Container()
+                          _tabIndex == 2
+                              ? CircleAvatar(
+                                  radius: 4,
+                                  backgroundColor: Theme.of(context).accentColor,
+                                )
+                              : Container()
                         ],
                       ),
                     )),
