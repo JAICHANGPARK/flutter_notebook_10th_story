4b8c64601b5fb1fd76aa65063aca9fcae19ed44b
Dreamwalker <aristojeff@gmail.com>
2021-01-31 18:24:22 +0900

    Update social_profile_page.dart

--- lib/ep683_social_network_app/ui/social_profile_page.dart
+++ lib/ep683_social_network_app/ui/social_profile_page.dart
@@ -12,35 +12,37 @@ class _SocialProfilePageState extends State<SocialProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SlidingUpPanel(
-        header: Text("Hello"),
-        footer: Text("Hello"),
-        body: Stack(
-          children: [
-            Positioned(child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage("https://cdn.pixabay.com/photo/2019/11/30/17/53/girl-4663792_960_720.jpg"),
-                  fit: BoxFit.cover,
-                  colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.35),
+      body: SafeArea(
+        child: SlidingUpPanel(
+          header: Text("Hello"),
+          footer: Text("Hello"),
+          body: Stack(
+            children: [
+              Positioned(child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage("https://cdn.pixabay.com/photo/2019/11/30/17/53/girl-4663792_960_720.jpg"),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.35),
 
-                  BlendMode.darken)
-                )
+                    BlendMode.darken)
+                  )
+                ),
               ),
-            ),
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,),
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,),
 
-          ],
+            ],
+          ),
+          panel: Column(
+            children: [
+
+            ],
+          ),
+
         ),
-        panel: Column(
-          children: [
-
-          ],
-        ),
-
       ),
     );
   }
