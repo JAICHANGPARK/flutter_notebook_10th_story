4c7217a97e4bd2784e55d56fbc5f82417986bb99
Dreamwalker <aristojeff@gmail.com>
2021-01-31 18:20:50 +0900

    Update social_profile_page.dart

--- lib/ep683_social_network_app/ui/social_profile_page.dart
+++ lib/ep683_social_network_app/ui/social_profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class SocialProfilePage extends StatefulWidget {
   SocialProfilePage({Key key}) : super(key: key);
@@ -10,6 +11,20 @@ class SocialProfilePage extends StatefulWidget {
 class _SocialProfilePageState extends State<SocialProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SlidingUpPanel(
+        body: Stack(
+          children: [
+
+          ],
+        ),
+        panel: Column(
+          children: [
+
+          ],
+        ),
+
+      ),
+    );
   }
 }
