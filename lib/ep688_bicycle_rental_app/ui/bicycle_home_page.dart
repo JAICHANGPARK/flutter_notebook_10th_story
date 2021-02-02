8bc6110b700d9bc6dcd91380c97b1ab0851e3f15
Dreamwalker <aristojeff@gmail.com>
2021-02-02 20:03:44 +0900

    Update bicycle_home_page.dart

--- lib/ep688_bicycle_rental_app/ui/bicycle_home_page.dart
+++ lib/ep688_bicycle_rental_app/ui/bicycle_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
 
 
 class BicycleRentalHomePage extends StatefulWidget {
@@ -18,26 +19,40 @@ class _BicycleRentalHomePageState extends State<BicycleRentalHomePage> {
           ],
         ),
       ),
-      bottomNavigationBar: GNav(
-          tabs: [
-            GButton(
-              icon: Icon(Icons.home)
-              text: 'Home',
-            ),
-            GButton(
-              icon: LineIcons.heart_o,
-              text: 'Likes',
-            ),
-            GButton(
-              icon: LineIcons.search,
-              text: 'Search',
-            ),
-            GButton(
-              icon: LineIcons.user,
-              text: 'Profile',
-            )
-          ]
-      ),
+      bottomNavigationBar:BottomAppBar(
+
+        child: Container(
+          height: 72,
+          color: Colors.white,
+          child: GNav(
+              curve: Curves.easeOutExpo, // tab animation curves
+              duration: Duration(milliseconds: 900), // tab animation duration
+              gap: 8, // the tab button gap between icon and text
+              activeColor: Colors.purple, // selected icon and text color
+              iconSize: 24, // tab button icon size
+              tabBackgroundColor: Colors.purple.withOpacity(0.1), // selected tab background color
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
+              tabs: [
+                GButton(
+                  icon: LineIcons.home,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: LineIcons.heart_o,
+                  text: 'Likes',
+                ),
+                GButton(
+                  icon: LineIcons.search,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: LineIcons.user,
+                  text: 'Profile',
+                )
+              ]
+          ),
+        ),
+      )
     );
   }
 }
