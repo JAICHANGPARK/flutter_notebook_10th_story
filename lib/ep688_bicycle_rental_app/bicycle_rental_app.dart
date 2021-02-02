8c579dffec05cbda890f9732ae7a684bead7e9dc
Dreamwalker <aristojeff@gmail.com>
2021-02-02 19:57:41 +0900

    Update bicycle_rental_app.dart

--- lib/ep688_bicycle_rental_app/bicycle_rental_app.dart
+++ lib/ep688_bicycle_rental_app/bicycle_rental_app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'ui/bicycle_home_page.dart';
+
 
 class BicycleRentalApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BicycleHomePage(),
+      home: BicycleRentalHomePage(),
     );
   }
 }
