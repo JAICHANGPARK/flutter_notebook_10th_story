672d42dff1ca291dd4021aab077c8522352c72eb
Dreamwalker <aristojeff@gmail.com>
2021-02-02 19:57:09 +0900

    Create bicycle_home_page.dart

---
+++ lib/ep688_bicycle_rental_app/ui/bicycle_home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class BicycleRentalHomePage extends StatefulWidget {
+  @override
+  _BicycleRentalHomePageState createState() => _BicycleRentalHomePageState();
+}
+
+class _BicycleRentalHomePageState extends State<BicycleRentalHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
