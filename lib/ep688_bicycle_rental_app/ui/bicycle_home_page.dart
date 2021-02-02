9d227512e1fd46ccfb45fe9d814f789b08e3e410
Dreamwalker <aristojeff@gmail.com>
2021-02-02 20:04:38 +0900

    Update bicycle_home_page.dart

--- lib/ep688_bicycle_rental_app/ui/bicycle_home_page.dart
+++ lib/ep688_bicycle_rental_app/ui/bicycle_home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 
-
 class BicycleRentalHomePage extends StatefulWidget {
   @override
   _BicycleRentalHomePageState createState() => _BicycleRentalHomePageState();
@@ -10,55 +9,59 @@ class BicycleRentalHomePage extends StatefulWidget {
 
 class _BicycleRentalHomePageState extends State<BicycleRentalHomePage> {
   int bottomIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-
-          ],
-        ),
-      ),
-      bottomNavigationBar:BottomAppBar(
-
-        child: Container(
-          height: 72,
-          color: Colors.white,
-          child: GNav(
-            selectedIndex: bottomIndex,
-              onTabChange: (idx){
-              
-              },
-              curve: Curves.easeOutExpo, // tab animation curves
-              duration: Duration(milliseconds: 900), // tab animation duration
-              gap: 8, // the tab button gap between icon and text
-              activeColor: Colors.purple, // selected icon and text color
-              iconSize: 24, // tab button icon size
-              tabBackgroundColor: Colors.purple.withOpacity(0.1), // selected tab background color
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5), // navigation bar padding
-              tabs: [
-                GButton(
-                  icon: LineIcons.home,
-                  text: 'Home',
-
-                ),
-                GButton(
-                  icon: LineIcons.heart_o,
-                  text: 'Likes',
-                ),
-                GButton(
-                  icon: LineIcons.search,
-                  text: 'Search',
-                ),
-                GButton(
-                  icon: LineIcons.user,
-                  text: 'Profile',
-                )
-              ]
+        body: SingleChildScrollView(
+          child: Column(
+            children: [],
           ),
         ),
-      )
-    );
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 72,
+            color: Colors.white,
+            child: GNav(
+                selectedIndex: bottomIndex,
+                onTabChange: (idx) {
+                  setState(() {
+                    bottomIndex = idx;
+                  });
+                },
+                curve: Curves.easeOutExpo,
+                // tab animation curves
+                duration: Duration(milliseconds: 900),
+                // tab animation duration
+                gap: 8,
+                // the tab button gap between icon and text
+                activeColor: Colors.purple,
+                // selected icon and text color
+                iconSize: 24,
+                // tab button icon size
+                tabBackgroundColor: Colors.purple.withOpacity(0.1),
+                // selected tab background color
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                // navigation bar padding
+                tabs: [
+                  GButton(
+                    icon: LineIcons.home,
+                    text: 'Home',
+                  ),
+                  GButton(
+                    icon: LineIcons.heart_o,
+                    text: 'Likes',
+                  ),
+                  GButton(
+                    icon: LineIcons.search,
+                    text: 'Search',
+                  ),
+                  GButton(
+                    icon: LineIcons.user,
+                    text: 'Profile',
+                  )
+                ]),
+          ),
+        ));
   }
 }
