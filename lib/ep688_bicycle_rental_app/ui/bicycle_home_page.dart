e001acb7f5fe7258bbe28e11e555b1294361816f
Dreamwalker <aristojeff@gmail.com>
2021-02-03 20:32:57 +0900

    Update bicycle_home_page.dart

--- lib/ep688_bicycle_rental_app/ui/bicycle_home_page.dart
+++ lib/ep688_bicycle_rental_app/ui/bicycle_home_page.dart
@@ -19,6 +19,7 @@ class _BicycleRentalHomePageState extends State<BicycleRentalHomePage> {
 
           child: SingleChildScrollView(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
                   padding: const EdgeInsets.all(16.0),
@@ -36,33 +37,39 @@ class _BicycleRentalHomePageState extends State<BicycleRentalHomePage> {
                     ],
                   ),
                 ),
-                Text.rich(TextSpan(children: [
-                  TextSpan(
-                    text: "Select ",style: TextStyle(
-                      fontSize: 24
-                  )
-                  ),
-                  TextSpan(
-                      text: "Bicycle",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                          fontSize: 24
-                      ))
-                ])),
-                Text.rich(TextSpan(children: [
-                  TextSpan(
-                      text: "To Riding",style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24
-                  )
-                  ),
-                  TextSpan(
-                      text: " Now",
-                      style: TextStyle(
-
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text.rich(TextSpan(children: [
+                    TextSpan(
+                      text: "Select ",style: TextStyle(
                         fontSize: 24
-                      ))
-                ])),
+                    )
+                    ),
+                    TextSpan(
+                        text: "Bicycle",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                            fontSize: 24
+                        ))
+                  ])),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Text.rich(TextSpan(children: [
+                    TextSpan(
+                        text: "To Riding",style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24
+                    )
+                    ),
+                    TextSpan(
+                        text: " Now",
+                        style: TextStyle(
+
+                          fontSize: 24
+                        ))
+                  ])),
+                ),
                 Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Container(
