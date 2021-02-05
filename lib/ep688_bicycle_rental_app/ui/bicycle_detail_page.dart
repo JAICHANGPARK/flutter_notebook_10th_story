7d488eb8790045231c04be36bf7dcdfd9eb1cad2
Dreamwalker <aristojeff@gmail.com>
2021-02-05 20:36:34 +0900

    Update bicycle_detail_page.dart

---
+++ lib/ep688_bicycle_rental_app/ui/bicycle_detail_page.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class BicycleDetailPage extends StatefulWidget {
+  @override
+  _BicycleDetailPageState createState() => _BicycleDetailPageState();
+}
+
+class _BicycleDetailPageState extends State<BicycleDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
