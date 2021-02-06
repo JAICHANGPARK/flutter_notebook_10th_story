08233a6939d4e004d980687fc2ec191169d0607a
Dreamwalker <aristojeff@gmail.com>
2021-02-06 20:02:16 +0900

    Update investing_concept_app.dart

---
+++ lib/ep692_investing_concept_app/investing_concept_app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class InvestingConceptApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
