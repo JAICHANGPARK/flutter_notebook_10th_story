6e2500de97f9e623cf3f960672a442e5c102a979
Dreamwalker <aristojeff@gmail.com>
2021-02-06 20:05:37 +0900

    Update investing_welcome_page.dart

--- lib/ep692_investing_concept_app/ui/investing_welcome_page.dart
+++ lib/ep692_investing_concept_app/ui/investing_welcome_page.dart
@@ -9,10 +9,20 @@ class _InvestingWelcomePageState extends State<InvestingWelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue[50],
       body: SafeArea(
         child: Stack(
           children: [
-            
+            Positioned(child: Container(
+              height: MediaQuery.of(context).size.height / 3,
+              decoration: BoxDecoration(
+                color: Colors.white
+              ),
+            ),
+            top: 0,
+            left: 0,
+            right: 0,
+            ),
           ],
         ),
       ),
