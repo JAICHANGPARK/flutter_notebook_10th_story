6e0d23cd04a5ec3bd4aece45e9cabd0ac9ab90c9
Dreamwalker <aristojeff@gmail.com>
2021-02-06 20:03:40 +0900

    Create investing_welcome_page.dart

---
+++ lib/ep692_investing_concept_app/ui/investing_welcome_page.dart
@@ -0,0 +1,11 @@
+class InvestingWelcomePage extends StatefulWidget {
+  @override
+  _InvestingWelcomePageState createState() => _InvestingWelcomePageState();
+}
+
+class _InvestingWelcomePageState extends State<InvestingWelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
