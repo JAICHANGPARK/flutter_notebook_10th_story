01c8557ab20e59ebca85c08ae7061f82d32b1a5b
Dreamwalker <aristojeff@gmail.com>
2021-02-06 20:13:55 +0900

    Update investing_welcome_page.dart

--- lib/ep692_investing_concept_app/ui/investing_welcome_page.dart
+++ lib/ep692_investing_concept_app/ui/investing_welcome_page.dart
@@ -24,47 +24,56 @@ class _InvestingWelcomePageState extends State<InvestingWelcomePage> {
         child: Stack(
           children: [
             Positioned(
-              child: Container(
-                height: MediaQuery.of(context).size.height / 4,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(16),
-                      topLeft: Radius.circular(16),
-                    )),
-                padding: EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Welcome!",
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
+              child: PhysicalModel(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(16),
+                  topLeft: Radius.circular(16),
+                ),
+                elevation: 4,
+                color: Colors.grey,
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 4,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(16),
+                        topLeft: Radius.circular(16),
+                      )),
+                  padding: EdgeInsets.all(8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Welcome!",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        IconButton(icon: Icon(Icons.clear), onPressed: () {})
-                      ],
-                    ),
-                    Text("Choose your interests to follow and trade"),
-                    Text("on your terms."),
-                    Container(
-                      height: 48,
-                      decoration: BoxDecoration(border: Border.all(color: Colors.blue[50], width: 1.2)),
-                      padding: EdgeInsets.symmetric(horizontal: 8),
-                      child: TextField(
-                        controller: _textEditingController,
-                        decoration: InputDecoration(
-                            icon: Icon(Icons.search),
-                            hintText: "Search interests to follow...",
-                            border: InputBorder.none),
+                          IconButton(icon: Icon(Icons.clear), onPressed: () {})
+                        ],
                       ),
-                    )
-                  ],
+                      Text("Choose your interests to follow and trade"),
+                      Text("on your terms."),
+                      Container(
+                        height: 48,
+                        decoration: BoxDecoration(border: Border.all(color: Colors.blue[50], width: 1.2)),
+                        padding: EdgeInsets.symmetric(horizontal: 8),
+                        child: TextField(
+                          controller: _textEditingController,
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.search),
+                              hintText: "Search interests to follow...",
+                              hintStyle: TextStyle(fontSize: 14, color: Colors.blue[100]),
+                              border: InputBorder.none),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
               top: 16,
