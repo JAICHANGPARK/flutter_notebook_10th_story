18b144c125a9a81a21bf191f7ddc49b86b536af4
Dreamwalker <aristojeff@gmail.com>
2021-02-11 17:38:41 +0900

    Update friendy_reservation_page.dart

---
+++ lib/ep697_friendy_app/ui/friendy_reservation_page.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class ReservationPage extends StatefulWidget {
+  @override
+  _ReservationPageState createState() => _ReservationPageState();
+}
+
+class _ReservationPageState extends State<ReservationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
