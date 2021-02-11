2fa13ac432bc1d5c4a66edabddc35aaf0a048b18
Dreamwalker <aristojeff@gmail.com>
2021-02-11 17:36:10 +0900

    Update friendy_app.dart

--- lib/ep697_friendy_app/friendy_app.dart
+++ lib/ep697_friendy_app/friendy_app.dart
@@ -1,1 +1,27 @@
 import 'package:flutter/material.dart';
+
+
+class FriendyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/" : 
+      }, 
+    );
+  }
+}
+
+
+class ReservationPage extends StatefulWidget {
+  @override
+  _ReservationPageState createState() => _ReservationPageState();
+}
+
+class _ReservationPageState extends State<ReservationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
