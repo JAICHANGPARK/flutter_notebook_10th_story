a530866734a3d3554cf0623d8a07f94bf85792ad
Dreamwalker <aristojeff@gmail.com>
2021-02-11 17:38:05 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'ep692_investing_concept_app/investing_concept_app.dart';
+import 'ep697_friendy_app/friendy_app.dart';
 
 void main() {
-  runApp(InvestingConceptApp());
+  runApp(FriendyApp());
 }
