bbd07d30743b5d28640c87d5b845507e662fca76
Dreamwalker <aristojeff@gmail.com>
2021-02-13 18:50:24 +0900

    Update friendy_reservation_page.dart

--- lib/ep697_friendy_app/ui/friendy_reservation_page.dart
+++ lib/ep697_friendy_app/ui/friendy_reservation_page.dart
@@ -13,6 +13,11 @@ class _ReservationPageState extends State<ReservationPage> {
   var items = [
     [false, true, true, false, false, false],
     [false, false, true, true, true, true, true, false],
+    [false, false, true, true, true, true, false, false],
+    [false, false, true, true, true, true, false, false],
+    [false, false, true, true, true, true, false, false],
+    [false, true, true, true, false, false, false, false],
+    [false, true, true, false, false, false],
   ];
 
   @override
